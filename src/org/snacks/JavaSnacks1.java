371282c95851f397834a690ae4acd672defd57e8
DarioGiusino <dario.giusino@gmail.com>
2023-05-05 10:39:45 +0200

    first commit, first snack

---
+++ src/org/snacks/JavaSnacks1.java
@@ -0,0 +1,18 @@
+package org.snacks;
+import java.util.Scanner;
+
+public class JavaSnacks1 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Inserisci un numero...");
+		int userNumber = scanner.nextInt();
+		scanner.close();
+		
+		String result = userNumber % 2 == 0 ? "" + userNumber : "" + (userNumber + 1);
+		
+		System.out.println(result);
+		
+	}
+}
